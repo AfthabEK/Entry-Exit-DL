7b8dbb158cb5b1866eabb8fab5b204dec0852072
Afthab <mohamedafthabek@gmail.com>
2023-01-27 21:01:19 +0530

    Modified URLs

--- entryexit/urls.py
+++ entryexit/urls.py
@@ -5,6 +5,11 @@ from enter import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.library, name='library'),
+    path('/', views.library, name='library'),
+    path('home', views.library, name='library'),
+    path('home/', views.library, name='library'),
+    path('library', views.library, name='library'),
     path('library/', views.library, name='library'),
     path('records/', views.records, name='records'),
     path('allrecords/', views.allrecords, name='allrecords'),
